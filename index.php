408de21d4d00716a423e6907a9d351b8ba150c31
kehomies <petri.saarimaa@gmail.com>
2013-10-31 18:44:21 +0200

    uutta
    uttta

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+<html> 
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"></meta>
+<title>MOI</title> 
+</head> 
+<body> 
+<!-- hello.php --> 
+<?php
+
+    echo "päivää";
 
 
-echo 'jööö!';
-
+?> 	
+</body> 
+</html>
